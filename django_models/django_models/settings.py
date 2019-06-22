6b886274bff74099e34edfce6afe3e7344054655
= <=>
2019-06-23 01:39:07 +0530

    added mysql connection details

--- django_models/django_models/settings.py
+++ django_models/django_models/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'django_models.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_models',
+        'HOST':'localhost',
+        'PORT': '3306',
+        'USER':'root',
+        'PASSWORD':'admin'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
